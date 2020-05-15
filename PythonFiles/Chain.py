17f3ef49b1b6ae13f0d3b55eb22f849000d485d1
guy <guybos@gmail.com>
2020-05-16 00:57:11 +0300

    chain in progress

--- PythonFiles/Chain.py
+++ PythonFiles/Chain.py
@@ -20,53 +20,72 @@ class Chain:
         num = 0
         newBlocks = self.getDbBlocks(self)
         for block in newBlocks:
-            if self.proofOfWork(block) != 1:
-                newBlock = self.resolveConflicts(block)
-                self.updateBlock(newBlock)
-            num = block.Id
+             self.currentHush = block[1]
+             answer = self.proofOfWork(block[0])
+             if answer['ResponseType'] == 0:
+                 newBlock = Block(block[0], block[2])
+                 self.currentHush = self.Hash(newBlock)
+                 if self.currentHush != block[1]:
+                     deleteblockTransactions(newBlock.Id)
+                     addTransaction(newBlock)
+                     updateBlock(newBlock)
+                     self.sendHash(newBlock.Id)
+             elif answer['ResponseType'] == 2:
+                 newBlock = Block(block[0], block[2])
+                 self.resolveConflicts(newBlock, False)
+             elif answer['ResponseType'] == 3:
+                self.sendTransactions(block[0])
+        num = block.Id
         for i in range(num + 1, lastBlockId):
-            newBlock = self(i, self.blocks[-1].hush)
-            checkResult = self.proofOfWork(newBlock)
-            if checkResult == 1:
-                self.addBlock(newBlock)
-            elif checkResult == 0:
-                self.blocks.append(newBlock)
-                self.addBlock(newBlock)
-                self.sendData(newBlock)
-            else:
-                self.resolveConflicts(newBlock)
-        return True
+            newBlock = (i, self.currentHush)
+            self.currentHush = self.Hash(newBlock)
+            answer = self.proofOfWork(i)
+            if answer['ResponseType'] == 0:
+                 addBlock(newBlock)
+                 self.sendHash(newBlock.Id) #bank Id, blockId ,currentHash
+            elif answer['ResponseType'] == 1:
+                 addBlock(newBlock)
+            elif answer['ResponseType'] == 2:
+                 self.resolveConflicts(newBlock, True)
+            elif answer['ResponseType'] == 3:
+                addBlock(newBlock)
+                self.sendTransactions(block[0])
 
     # changes data in block and DB by the consensus
-    def resolveConflicts(self, block):
-        temp = url + "Transaction/CheckConsensus"
-        payload = {'bankId': self.BankId, 'BlockchainNumber': block.Id}
-        headers = {'Content-Type': 'application/json'}
-    # replace with method - delete from table,check block, insert to table
-        try:
-            with no_ssl_verification():
-                response = requests.request('GET', url=temp, headers=headers, data=json.dumps(payload))
-                if response['Hash'] == self.hash:
-                    return block
-                else:
-                    return response
-        except requests.exceptions.RequestException as e:
-            print(e)
-        newBlock = Block()
-        return True
+    def resolveConflicts(self, block, isNew):
+        self.currentHush = self.Hash(block)
+        answer = self.proofOfWork(block.Id)
+        if answer['ResponseType'] == 1:
+            deleteblockTransactions(block.Id)
+            addTransaction(block)
+            if isNew:
+                addBlock(block)
+            else:
+                updateBlock(block)
+            self.sendHash(block.Id)
+        if answer['ResponseType'] == 2:
+            temp = url + "Transaction/CheckConsensus"
+            payload = {'BlockchainNumber': block.Id}
+            headers = {'Content-Type': 'application/json'}
+            try:
+                with no_ssl_verification():
+                    response = requests.request('GET', url=temp, headers=headers, data=json.dumps(payload))
+                    if response.status_code == 200:
+                        self.checkTransactions(response['transactionList'])
+            except requests.exceptions.RequestException as e:
+                print(e)
 
     # a function to check that the block is in-sync with server
-    def proofOfWork(self,block):
+    def proofOfWork(self, blockId):
         temp = url + "Transaction/CheckHash/"
-        payload = {'BlockchainNumber': block.Id, 'Hush': self.currentHash}
+        payload = {'BlockchainNumber': blockId, 'Hush': self.currentHash}
         headers = {'Content-Type': 'application/json'}
         try:
             with no_ssl_verification():
                 response = requests.request('GET', url=temp, headers=headers, data=json.dumps(payload))
-                return response
+                return response.json()
         except requests.exceptions.RequestException as e:
             print(e)
-
 
     # a function to check if block is ready to be filled with data and have a consensus
     def isBlockReady(self):
@@ -79,29 +98,41 @@ class Chain:
             return -1
         return response.json()
 
-    def checkTransactions(self):
+    #deletes or adds transactions per consensus
+    def checkTransactions(self, TrasactionList):
         return True
 
-    def Hash(self, index):
-        block_string = json.dumps(self.blocks[index]).encode()
-        self.currentHash = hashlib.sha256(block_string).hexdigest()
+    def Hash(self, newBlock):
+        block_string = json.dumps(newBlock).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def sendHash(self, blockId):
+        pass
+
+    def sendTransactions(self, blockId):
+        pass
 
 
-# Need to Fix all repeated code and use a function to call querys/sp
 # Adds the new block to this bankDb
 def addBlock(block):
+    args = [block.Id, block.hush, block.previousBlockHash]
+    sp ='addBlock'
+    answer =UseDb(args, sp)
+    addTransaction(block)
+
+
+#  Adds transacionList to transactionperblock Table
+def addTransaction(block):
     blockList = list()
     connection = connect()
     cursor = connection.cursor()
-    args = [block.Id, block.hush, block.previousBlockHash]
-    cursor.callproc('addBlock',args)
     query = "INSERT INTO transactionsperblock (transactionId, blockId) VALUES(%s, %s)"
     for transaction in block.data:
         args = [transaction['transactionId'],block.Id]
         blockList.append(args)
     cursor.executemany(query, blockList)
     connection.commit
-    cursor.close()
+
 
 # gets all the data of the blocks in the bankDb
 def getDbBlocks():
@@ -112,18 +143,18 @@ def getDbBlocks():
 
 
 # Updates the block in this bankDb
-def updateBlock( block):
+def updateBlock(block):
     args = [block.Id, block.hush, block.previousBlockHash]
     sp = 'updateBlock'
     UseDb(args, sp)
 
-
+# Deletes all transaction that were in this block.
 def deleteblockTransactions(blockId):
     sp = 'deleteTransactions'
     args = [blockId]
     answer = UseDb(args, sp)
 
-
+# A function get procedure name and argumens and use the stored procedure in his BankDB.
 def UseDb(args, sp):
     connection = connect()
     cursor = connection.cursor()
